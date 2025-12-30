b6bca12da77eb7925f8ce3e691095d8bedbb5cd2
Zaidan Abdul Aziz <121195413+Zabduziz@users.noreply.github.com>
2025-12-30 22:18:44 +0700

    Add realtime function for live detection

--- Yolo.py
+++ Yolo.py
@@ -5,5 +5,9 @@ def main():
     results = model("gallery-15.jpg")  # deteksi
     results[0].show()  # tampilkan hasil
 
+def realtime():
+    model = YOLO("yolov8n.pt")
+    model(source=0, show=True)
+
 if __name__ == "__main__":
     main()
